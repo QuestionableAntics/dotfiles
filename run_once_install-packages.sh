9a71b308300cf6cdb0b749399699bfb8fcc1fe05
Kean Mattingly <kean.mattingly@equipmentshare.com>
2022-11-24 19:44:04 -0600

    documentation for what needs to be auto installed

---
+++ run_once_install-packages.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# install asdf
+# git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+
+# asdf plugin add neovim
+# asdf install neovim nightly
+# asdf global neovim nightly
+
+# asdf plugin add deno https://github.com/asdf-community/asdf-deno.git
+# asdf install deno latest
+# asdf global deno latest
+
+# asdf plugin add lazydocker https://github.com/comdotlinux/asdf-lazydocker.git
+# asdf install lazydocker latest
+# asdf global lazydocker latest
