57d1a0fadde5d067e3c1230dce60a1503c7f2b5b
Kean Mattingly <kean.mattingly@equipmentshare.com>
2023-07-15 12:45:22 -0500

    correct git clone

--- run_once_b_common.sh
+++ run_once_b_common.sh
@@ -11,27 +11,27 @@ pip install pynvim
 
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 cargo binstall rtx-cli -y
-source $HOME/.zshrc \
-	&& rtx plugin add neovim \
-	&& rtx plugin add dotnet-core https://github.com/emersonsoares/asdf-dotnet-core.git \
-	&& rtx plugin add lazydocker https://github.com/comdotlinux/asdf-lazydocker.git \
-	&& rtx plugin add lazygit https://github.com/nklmilojevic/asdf-lazygit.git \
-	&& rtx plugin add bat \
-	&& rtx plugin add golang https://github.com/kennyp/asdf-golang.git \
-	&& rtx use -g neovim@nightly \
-	&& rtx use -g dotnet-core@7.0.304 \
-	&& rtx use -g nodejs@20.3.1 \
-	&& rtx use -g python@3.9.12 \
-	&& rtx use -g lazydocker@latest \
-	&& rtx use -g lazygit@latest \
-	&& rtx use -g bat@latest \
-	&& rtx use -g golang@latest \
-	&& rtx use -g ripgrep@latest \
-	&& rtx use -g fzf@latest \
-	&& rtx use -g fd@latest \
-	&& rtx use -g poetry@latest \
-	&& rtx use -g awscli@latest \
-	&& rtx use -g lua@latest
+# source $HOME/.zshrc
+rtx plugin add neovim
+rtx plugin add dotnet-core https://github.com/emersonsoares/asdf-dotnet-core.git
+rtx plugin add lazydocker https://github.com/comdotlinux/asdf-lazydocker.git
+rtx plugin add lazygit https://github.com/nklmilojevic/asdf-lazygit.git
+rtx plugin add bat
+rtx plugin add golang https://github.com/kennyp/asdf-golang.git
+rtx use -g neovim@nightly
+rtx use -g dotnet-core@7.0.304
+rtx use -g nodejs@20.3.1
+rtx use -g python@3.9.12
+rtx use -g lazydocker@latest
+rtx use -g lazygit@latest
+rtx use -g bat@latest
+rtx use -g golang@latest
+rtx use -g ripgrep@latest
+rtx use -g fzf@latest
+rtx use -g fd@latest
+rtx use -g poetry@latest
+rtx use -g awscli@latest
+rtx use -g lua@latest
 
 eval "$(rtx env -s zsh)"
 
@@ -48,13 +48,13 @@ nvim --headless "Lazy sync" +"sleep 20" +q
 ################################################################################
 
 zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-clone --depth=1 \
+git clone --depth=1 \
 	https://github.com/zsh-users/zsh-syntax-highlighting.git \
 	$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting \
 # copy pre-oh-my-zsh contents to .zshrc
 # delete .zshrc and replace with pre-oh-my-zsh contents
-rm $HOME/.zshrc \
-mv $HOME/.zshrc.pre-oh-my-zsh $HOME/.zshrc \
+rm $HOME/.zshrc
+mv $HOME/.zshrc.pre-oh-my-zsh $HOME/.zshrc
 # install powerlevel10k
 git clone --depth=1 \
 	https://github.com/romkatv/powerlevel10k.git \
