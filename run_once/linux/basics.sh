afa70cd25cbe55864e0f4386dcf7395b505fa6b1
Kean Mattingly <kean.mattingly@equipmentshare.com>
2023-07-15 12:22:42 -0500

    run once separation

---
+++ run_once/linux/basics.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+apt-get update -y \
+	&& apt-get install -y \
+	apt-utils \
+	build-essential \
+	bzip2 \
+	cmake \
+	curl \
+	git \
+	gzip \
+	libbz2-dev \
+	libncurses5-dev \
+	libncursesw5-dev \
+	libffi-dev \
+	libjpeg-dev \
+	liblzma-dev \
+	libreadline8 \
+	libreadline-dev \
+	libsqlite3-dev \
+	libssl-dev \
+	locales \
+	lzma \
+	nginx \
+	openssl \
+	pkg-config \
+	python3-pip \
+	sqlite3 \
+	supervisor \
+	unzip \
+	wget \
+	yasm \
+	zlib1g-dev \
+	zsh
+
